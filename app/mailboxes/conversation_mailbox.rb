372070f6da74355b58b5032784a0dc2841d04df9
Anton Zaharia <zaharia_anton@yahoo.com>
2023-02-10 12:25:08 +0000

    conversation mailbox process

--- app/mailboxes/conversation_mailbox.rb
+++ app/mailboxes/conversation_mailbox.rb
@@ -1,4 +1,20 @@
 class ConversationMailbox < ApplicationMailbox
   def process
+    conversation = Conversation.create(subject: mail.subject)
+    conversation.posts.create(
+      contact: author,
+      body: body,
+      message_id: mail.message_id,
+    )
+  end
+
+  def author
+    @author ||= if (user = User.find_by(email: from.address))
+        user
+      else
+        contact = Contact.where(email: from.address).first_or_initialize
+        contact.update(name: from.display_name)
+        contact
+      end
   end
 end
